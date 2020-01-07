d6b9970fec460b90a672c794ca53edca9362dbbc
stellakaranikola <karanikolastella@hotmail.com>
2020-01-07 21:02:02 +0200

    fix https on jquere

--- unogame/index.php
+++ unogame/index.php
@@ -13,12 +13,11 @@ if (isset($_GET['logout'])) {
 }
 
 ?>
-<!DOCTYPE html>
 <html>
 <head>
     <title>Home</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
     <link rel="stylesheet" type="text/css" href="gamecss.css">
     <link rel="stylesheet" type="text/css" href="popUp.css">
 
