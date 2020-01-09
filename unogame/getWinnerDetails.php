0ec5b7ee0bad82668f978556e88342c9c898d113
stellakaranikola <karanikolastella@hotmail.com>
2020-01-09 22:06:08 +0200

    server fix

--- unogame/getWinnerDetails.php
+++ unogame/getWinnerDetails.php
@@ -5,7 +5,7 @@ include './globalFunctions.php';
 $currentGameName = $_SESSION['gamename'];
 $currentPlayerUserName = $_SESSION['username'];
 
-$sqlInsertWinner = "SELECT winnerID FROM gametowinner where gameName = '$currentGameName'";
+$sqlInsertWinner = "SELECT winnerID FROM gameToWinner where gameName = '$currentGameName'";
 $sql_query_result = mysqli_query($db, $sqlInsertWinner);
 $sql_query_result_first_row = mysqli_fetch_assoc($sql_query_result);
 $winnerID = $sql_query_result_first_row['winnerID'];
