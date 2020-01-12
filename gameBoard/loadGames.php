6c8a52817064f82a0fd14dbc2751e7e0f5d52c01
GeorgiaKem <georgiakem10@gmail.com>
2020-01-12 15:39:26 +0200

    Implement show number of players

--- gameBoard/loadGames.php
+++ gameBoard/loadGames.php
@@ -6,12 +6,22 @@
     while ($row = mysqli_fetch_assoc($result)) {
         $gameName = $row['gamename'];
         echo "<form class='.content' action='joingame.php' method='POST'>";
-        echo "   Game ID: ";
+        echo "   ID: ";
         echo "<b>";
         echo $row['gameid'];
         echo "</b>";
         echo "  Name: ";
         echo "<b>$gameName <b>    ";
+
+        // get players count
+        //gameid
+        $sql_to_get_NumberOfPlayers = "SELECT count(*) as countPlayers from gametousersconnection where gameName = '$gameName'";
+        $sql_result_NumberOfPlayers = mysqli_query($db, $sql_to_get_NumberOfPlayers);
+        $firstrow_sql_result_NumberOfPlayers = mysqli_fetch_assoc($sql_result_NumberOfPlayers);
+        $numberOfPlayers = $firstrow_sql_result_NumberOfPlayers['countPlayers'];
+
+        echo "<b> Players: $numberOfPlayers   "
+
         echo "<form class='.noBorder' action='joingame.php' method='POST'>";
         echo "<input name='gameName' placeholder='Game Name' value=$gameName hidden=true>";
         echo "<button type='submit' class='btn'> Join </button>";
