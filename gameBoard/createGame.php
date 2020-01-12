9798e2d3efa0362ffa89f2f6694b18d614495caa
GeorgiaKem <georgiakem10@gmail.com>
2020-01-12 15:26:57 +0200

    Fix bug so that user cant create game without name

--- gameBoard/createGame.php
+++ gameBoard/createGame.php
@@ -12,34 +12,38 @@
         $adminUserName = $_SESSION['username'];
         echo $adminUserName;
         $gameName = $_POST['gamename'];
-        $_SESSION['gamename'] = $gameName;
+        if (strcmp($cardThatJustPlayedValue,'') > 0) {
+            $_SESSION['gamename'] = $gameName;
 
-        $sql = "SELECT id FROM users WHERE username='$adminUserName'";
-        $resultID = mysqli_query($db, $sql);
-        $firstrow = mysqli_fetch_assoc($resultID);
-        $adminID = $firstrow['id'];
-        echo $adminID;
-        $_SESSION["userID"] = $adminID;
-        $_SESSION["isAdmin"] = true;
-
-
-        /*  2
-            create the game in games table with admin id the users id and default values to started and finished to false
-        */
-
-        $sql = "INSERT INTO games (adminid, gamename, finished, started) VALUES ('$adminID', '$gameName',false, false)";
-        $result = mysqli_query($db, $sql);
+            $sql = "SELECT id FROM users WHERE username='$adminUserName'";
+            $resultID = mysqli_query($db, $sql);
+            $firstrow = mysqli_fetch_assoc($resultID);
+            $adminID = $firstrow['id'];
+            echo $adminID;
+            $_SESSION["userID"] = $adminID;
+            $_SESSION["isAdmin"] = true;
 
 
-        /* 4
-            gametousersconnection keeps many to many assotiation between users and games
-            we insert the assotiation between the user and the game
-        */
-        $sql = "INSERT INTO gametousersconnection (userid, gameName) VALUES ('$adminID','$gameName')";
-        $resultID = mysqli_query($db, $sql);
+            /*  2
+                create the game in games table with admin id the users id and default values to started and finished to false
+            */
+
+            $sql = "INSERT INTO games (adminid, gamename, finished, started) VALUES ('$adminID', '$gameName',false, false)";
+            $result = mysqli_query($db, $sql);
+
+
+            /* 4
+                gametousersconnection keeps many to many assotiation between users and games
+                we insert the assotiation between the user and the game
+            */
+            $sql = "INSERT INTO gametousersconnection (userid, gameName) VALUES ('$adminID','$gameName')";
+            $resultID = mysqli_query($db, $sql);
 
 
 
-        header("location: ../unogame");
+            header("location: ../unogame");
+         } else {
+         
+         }
 
 ?>
